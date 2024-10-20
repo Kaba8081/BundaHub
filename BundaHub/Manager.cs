d8bb315c7b9e63dde0dbb4e765141c9cab92a9d9
Kaba <Kaba8081@gmail.com>
2024-10-20 11:55:39 +0200

    Feat. Sorting the inventory, basic display

--- BundaHub/Manager.cs
+++ BundaHub/Manager.cs
@@ -14,11 +14,43 @@ namespace BundaHub
                 new Item("Pen", 5, 3000),
                 new Item("Mug", 25, 130)
             };
+
+            _SortInventory("name", true);
+        }
+        private void _SortInventory(string sortBy, bool ascending = true)
+        {
+            switch (sortBy)
+            {
+            case "name":
+                // Sort the inventory by name
+                if (ascending) Array.Sort(_inventory, (x, y) => x.Name.CompareTo(y.Name));
+                else Array.Sort(_inventory, (x, y) => y.Name.CompareTo(x.Name));
+                
+                break;
+            case "price":
+                // Sort the inventory by price
+                if (ascending) Array.Sort(_inventory, (x, y) => x.Price.CompareTo(y.Price));
+                else Array.Sort(_inventory, (x, y) => y.Price.CompareTo(x.Price));
+
+                break;
+            case "quantity":
+                // Sort the inventory by quantity
+                if (ascending) Array.Sort(_inventory, (x, y) => x.Quantity.CompareTo(y.Quantity));
+                else Array.Sort(_inventory, (x, y) => y.Quantity.CompareTo(x.Quantity));
+
+                break;
+            default:
+                throw new ArgumentException("Invalid sortBy value. Allowed values are: name, price, quantity.");
+            }
         }
 
         public void ViewInventory()
         {
             Console.WriteLine("Viewing inventory...");
+            foreach (Item item in _inventory)
+            {
+                Console.WriteLine($"Name: {item.Name}, Price: {item.Price}, Quantity: {item.Quantity}, Total Price: {item.TotalPrice}");
+            }
         }
 
         public void AddItem()
