5480a1c2121d102548d47afae2d431ae2e6262a6
Kacper Baum <Kaba8081@gmail.com>
2024-11-20 17:23:07 +0100

    Feat. New project structure

---
+++ src/Shared/Domain/Item.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Domain
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public Item(string name) : this(name, 0, 1)
+        {
+        }
+        public Item(string name, decimal price) : this(name, price, 1)
+        {
+        }
+        public Item(string name, decimal price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            TotalPrice = price * quantity;
+        }
+    }
+}
