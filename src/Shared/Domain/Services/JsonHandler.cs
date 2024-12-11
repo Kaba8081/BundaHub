690b6ff6489f5bce3e0a74933480b8d5b0a049e7
Jakub Bąk <jbjakubbak@gmail.com>
2024-12-11 17:37:05 +0100

    Reapply "Chore. Added json serialization and deserialization"
    This reverts commit 76851ba5924ec89e7f59cf09d080d56f3fac4b71.

---
+++ src/Shared/Domain/Services/JsonHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Windows;
+
+namespace Domain.Services
+{
+    public class JsonHandler
+    {
+        public static T? LoadFromFile<T>(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                return default;
+            }
+
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(json, new JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                PropertyNameCaseInsensitive = true
+            });
+        }
+
+        public static void SaveToFile<T>(string filePath, T data)
+        {
+            var json = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
