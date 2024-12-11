76851ba5924ec89e7f59cf09d080d56f3fac4b71
Jakub Bąk <jbjakubbak@gmail.com>
2024-12-11 17:35:22 +0100

    Revert "Chore. Added json serialization and deserialization"
    This reverts commit 48fd6bd7ef82933a42f82a21935bb16c89fa14f7.

---
+++ src/BundaHubManager/Utilities/JsonHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BundaHubManager.Utilities
+{
+    public class JsonHelper
+    {
+        public static void SaveToJson<T>(IEnumerable<T> data, string filePath)
+        {
+            try
+            {
+                string json = JsonSerializer.Serialize(data, JsonOptionsProvider.GetDefaultOptions());
+                File.WriteAllText(filePath, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving data: {ex.Message}", "Save Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
--- src/Shared/Domain/Models/ItemModel.cs
+++ src/Shared/Domain/Models/ItemModel.cs
@@ -1,11 +1,15 @@
 ﻿using Domain.Entites;
-using System.Text.Json.Serialization;
 
 namespace Domain.Models
 {
-    public class ItemModel : Item
+    public class ItemModel: Item
     {
-        [JsonConstructor]
+        public ItemModel(string name) : this(name, 0, 1, [])
+        {
+        }
+        public ItemModel(string name, decimal price) : this(name, price, 1, [])
+        {
+        }
         public ItemModel(string name, decimal price, int quantity, ItemProperties[] properties)
         {
             Name = name;
@@ -14,17 +18,7 @@ namespace Domain.Models
             TotalPrice = price * quantity;
             Properties = properties;
         }
-
-        public ItemModel(string name) : this(name, 0, 1, Array.Empty<ItemProperties>())
-        {
-        }
-
-        public ItemModel(string name, decimal price) : this(name, price, 1, Array.Empty<ItemProperties>())
-        {
-        }
-
         public string[] GetProperties { get => GetItemProperties(); }
-
         private string[] GetItemProperties()
         {
             List<string> properties = new List<string>();
